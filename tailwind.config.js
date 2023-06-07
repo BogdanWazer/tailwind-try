1f4c4198a49cb78c5dd5fd321e887e248f265708
BogdanWazer <ki4ack.bogdan@gmail.com>
2023-06-07 21:05:28 +0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  darkMode: 'class',
   theme: {
     screens: {
-      'lg': {'max':'992px'},
-      'md': {'max':'768px'},
-      'sm': {'max':'480px'}
+      lg: { max: '992px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
     },
     container: {
       padding: '20px',
-      center: true
+      center: true,
     },
 
     extend: {
@@ -21,8 +22,8 @@ module.exports = {
         darkgray: '#272727',
       },
       boxShadow: {
-        'customred': '0px 0px 30px rgba(255, 13, 56, 0.21)'
-      }
+        customred: '0px 0px 30px rgba(255, 13, 56, 0.21)',
+      },
     },
   },
   plugins: [],
