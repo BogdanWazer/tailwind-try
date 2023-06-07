5a0b5470633f5f2fbdc11abf87fe0af512106678
Your Name <ki4ack.bogdan@gmail.com>
2023-06-07 15:52:31 +0300

    eeew2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'lg': {'max':'992px'},
+      'md': {'max':'768px'},
+      'sm': {'max':'480px'}
+    },
+    container: {
+      padding: '20px',
+      center: true
+    },
+
+    extend: {
+      colors: {
+        lightback: '#4d4244',
+        lightred: '#ff0d38',
+        darkred: '#d70026',
+        lightgray: '#747474',
+        darkgray: '#272727',
+      }
+    },
   },
   plugins: [],
-}
+};
 
